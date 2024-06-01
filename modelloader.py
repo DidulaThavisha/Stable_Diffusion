ba63ba276dd9777d3c86cb396cfebd4273f2e7e2
DidulaThavisha <86177477+DidulaThavisha@users.noreply.github.com>
2024-06-02 02:05:41 +0530

    modelloader

---
+++ modelloader.py
@@ -0,0 +1,30 @@
+from clip import CLIP
+from encoder import Encoder
+from decoder import Decoder
+from diffusion import Diffusion
+import modelconverter
+
+
+def load_weights(ckpt_path, device):
+    state_dict = modelconverter.load_from_standard_weights(ckpt_path, device)
+    encoder = Encoder().to(device)
+    encoder.load_state_dict(state_dict["encoder"], strict=True)
+
+    decoder = Decoder().to(device)
+    decoder.load_state_dict(state_dict["decoder"], strict=True)
+
+    diffusion = Diffusion().to(device)
+    diffusion.load_state_dict(state_dict["diffusion"], strict=True)
+
+    clip = CLIP().to(device)
+    clip.load_state_dict(state_dict["clip"], strict=True)
+
+    return {
+        "encoder": encoder,
+        "decoder": decoder,
+        "diffusion": diffusion,
+        "clip": clip,
+    }
+
+
+load_weights("data/v1-5-pruned-emaonly.ckpt", "cpu")
